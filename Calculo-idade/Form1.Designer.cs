59f313bcb0bf408df91559dc1c2e79de71bb8c38
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-04-19 20:56:21 -0300

    Adicionar arquivos de projeto.

---
+++ Calculo-idade/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Calculo_idade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dia";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(66, 88);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(148, 20);
+            this.txtDia.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mês";
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(66, 157);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(148, 20);
+            this.txtMes.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ano";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(66, 224);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(148, 20);
+            this.txtAno.TabIndex = 5;
+            this.txtAno.TextChanged += new System.EventHandler(this.txtAno_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(136, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Escreva abaixo sua data de nascimento";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(305, 222);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(312, 95);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 451);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Calculo-idade/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_idade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int diasAno;
+            int mes;
+            int idade;
+            if (Convert.ToInt32(txtAno.Text) <= 2023)
+            {
+                idade = 2023 - Convert.ToInt32(txtAno.Text);
+                diasAno = idade * 365;
+                if (Convert.ToInt32(txtMes.Text) < 13)
+                {
+                    mes = Convert.ToInt32(txtMes.Text) * 30;
+                    int resultado =(mes + diasAno) - Convert.ToInt32(txtDia.Text);
+                    lblResultado.Text = Convert.ToString(resultado);
+                }
+            } 
+        }
+
+        private void txtAno_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
